56e9f06bfab7507aa763ff915898707a4a16ed71
MartinVazquezFlexes <martin.vazquez@techforb.com>
2025-10-21 13:24:32 -0300

    FIX no commited part

--- projects/test-library/src/app/app.component.ts
+++ projects/test-library/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-library';
+
+    constructor(private translate: TranslateService) {
+    //configurar idiomas disponibles
+    translate.addLangs(['es', 'pt']);
+    
+    //establecer idioma por defecto
+    translate.setDefaultLang('es');
+    
+    //usar español (o detectar del navegador)
+    translate.use('es');
+  }
+
+  ngOnInit() {
+
+  }
 }
